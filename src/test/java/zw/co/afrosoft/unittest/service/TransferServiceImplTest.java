8b15dc2edee4312005e1bacd69833f5df361040e
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-04-10 22:18:55 +0200

    Added a few more test cases

--- src/test/java/zw/co/afrosoft/unittest/service/TransferServiceImplTest.java
+++ src/test/java/zw/co/afrosoft/unittest/service/TransferServiceImplTest.java
@@ -8,14 +8,18 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import zw.co.afrosoft.unittest.domain.Account;
+import zw.co.afrosoft.unittest.exceptions.AccountNotFoundException;
 import zw.co.afrosoft.unittest.repository.AccountRepository;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,6 +51,23 @@ class TransferServiceImplTest {
                 .changeAmount(1,new BigDecimal(900));
         verify(accountRepository)
                 .changeAmount(2,new BigDecimal(600));
+    }
+    @Test
+    @DisplayName("Test if it throws an Exception when destination account not found")
+    void transferMoneyDestinationAccountNotFoundFlow(){
+        Account sender = new Account();
+        sender.setId(1L);
+        sender.setAmount(new BigDecimal(5000));
+
+        given(accountRepository.findById(sender.getId()))
+                .willReturn(Optional.of(sender));
+        given(accountRepository.findById(2L))
+                .willReturn(Optional.empty());
+        assertThrows(AccountNotFoundException.class,()-> transferService.transferMoney(1L,2L,
+                new BigDecimal(300)));
+
+        verify(accountRepository,never())
+                .changeAmount(anyLong(),any());
     }
 
     @Test
